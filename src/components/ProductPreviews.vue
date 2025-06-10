4d4854fafb51c6c7bb838158eda0fb40b88e9f24
mrak <mate.rak@capgemini.com>
2025-06-10 09:27:20 +0200

    viewing product on card clicking

--- src/components/ProductPreviews.vue
+++ src/components/ProductPreviews.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-row v-if="filteredProducts.length">
       <v-col v-for="product in filteredProducts" :key="product.id" cols="12" sm="6" md="4" lg="3">
-        <v-card class="rounded-xl" elevation="3">
+        <v-card class="rounded-xl" elevation="3" @click="viewDetails(product.id)">
           <div class="image-container">
             <v-img :src="product.imageUrl || logoImage" class="logo-image" cover></v-img>
 
